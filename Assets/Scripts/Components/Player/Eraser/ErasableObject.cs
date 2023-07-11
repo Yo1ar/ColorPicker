959c8b5be0cdd4f777bc05674d148e979d1f22b1
Nikita Ivanin <nikitaivanin93@gmail.com>
2023-07-11 22:54:04 +0200

    done - NB-10_Add eraser mechanics https://trello.com/c/VblMKROX
    add - IErasable, ErasableObject scripts
    add - pfb_eraserAbove
    imp - highlight all erasable object
    imp - erase erasable object

---
+++ Assets/Scripts/Components/Player/Eraser/ErasableObject.cs
@@ -0,0 +1,38 @@
+using System;
+using GameCore.GameServices;
+using UnityEditor;
+using UnityEngine;
+using Utils;
+using Utils.Constants;
+
+namespace Components.Player.Eraser
+{
+	public class ErasableObject : MonoBehaviour, IErasable
+	{
+		[SerializeField] private GameObject _eraserAbove;
+		[SerializeField] private float _eraserAboveOffset;
+		
+
+		private void Awake()
+		{
+			_eraserAbove = Instantiate(_eraserAbove, transform.position.AddY(_eraserAboveOffset), Quaternion.identity);
+			_eraserAbove.transform.SetParent(transform);
+			Highlight(false);
+		}
+
+		private void Start() =>
+			ServiceLocator.factoryService.AddToErasable(this);
+
+		public void Erase() =>
+			gameObject.SetActive(false);
+
+		public void Highlight(bool value) =>
+			_eraserAbove.SetActive(value);
+
+		private void OnDrawGizmosSelected()
+		{
+			Gizmos.color = Colors.GreenT;
+			Gizmos.DrawCube(transform.position.AddY(_eraserAboveOffset), Vector3.one / 2);
+		}
+	}
+}
---
+++ Assets/Scripts/Components/Player/Eraser/IErasable.cs
@@ -0,0 +1,8 @@
+namespace Components.Player.Eraser
+{
+	public interface IErasable
+	{
+		void Erase();
+		void Highlight(bool value);
+	}
+}
