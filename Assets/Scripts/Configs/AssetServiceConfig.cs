fefe3ff27ed82345623ef1998690e5ba21cfdca4
Nikita Ivanin <nikitaivanin93@gmail.com>
2023-07-18 21:48:41 +0200

    finish: [NB-22] Fix player graphics mod: add container for player view rem: DrawingHand.cs

--- Assets/Scripts/Configs/AssetServiceConfig.cs
+++ Assets/Scripts/Configs/AssetServiceConfig.cs
@@ -1,5 +1,4 @@
 ﻿using Components;
-using Components.Level.Draw;
 using UnityEngine;
 
 namespace Configs
@@ -7,7 +6,6 @@ namespace Configs
 	[CreateAssetMenu(fileName = "AssetConfig", menuName = "Configs/Asset config")]
 	public sealed class AssetServiceConfig : ScriptableObject
 	{
-		public DrawingHand DrawingHand;
 		public GameObject Player;
 		public Projectile FireballProjectile;
 		public Projectile PencilProjectile;
