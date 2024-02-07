065aef049a2942e9b2098b025e2a16d4634df348
Nikita Ivanin <nikitaivanin93@gmail.com>
2024-02-07 18:37:16 +0100

    add skillBar

--- Assets/Code/UI/SkillBarNew.cs
+++ Assets/Code/UI/SkillBarNew.cs
@@ -1,11 +1,12 @@
 ﻿using System;
-using BgTools.Utils;
+using System.Collections.Generic;
 using Characters.Player;
 using GameCore.GameServices;
-using UI;
+using UnityEngine;
 using UnityEngine.UIElements;
 using Utils;
 using Utils.Constants;
+using SystemInfo = UnityEngine.Device.SystemInfo;
 
 namespace UI
 {
@@ -30,6 +31,7 @@ namespace UI
 		private const string k_fireballButton = "btn_fireball";
 		private const string k_eraserButton = "btn_eraser";
 		private const string k_speedButton = "btn_speed";
+		private const string k_hint = "hint";
 
 		private readonly Button _jumpButton;
 		private readonly Button _fireballButton;
@@ -47,10 +49,15 @@ namespace UI
 			_factoryService = Services.FactoryService;
 			RootElement = document.rootVisualElement.GetVisualElement(k_skillbar);
 
-			_jumpButton = RootElement.GetVisualElement(k_jumpButton).GetButton();
-			_fireballButton = RootElement.GetVisualElement(k_fireballButton).GetButton();
-			_eraserButton = RootElement.GetVisualElement(k_eraserButton).GetButton();
-			_speedButton = RootElement.GetVisualElement(k_speedButton).GetButton();
+			_jumpButton = RootElement.GetButton(k_jumpButton);
+			_fireballButton = RootElement.GetButton(k_fireballButton);
+			_eraserButton = RootElement.GetButton(k_eraserButton);
+			_speedButton = RootElement.GetButton(k_speedButton);
+
+			List<VisualElement> hints = RootElement.Query(k_hint).ToList();
+			if (SystemInfo.deviceType != DeviceType.Desktop)
+				foreach (VisualElement hint in hints)
+					hint.visible = false;
 
 			if (_factoryService.Player)
 				GetPlayerColorHolder();
@@ -75,7 +82,7 @@ namespace UI
 			_eraserButton.visible = false;
 			_speedButton.visible = false;
 
-			_jumpButton.style.color = new StyleColor(Colors.GetColor(EColors.White));
+			_jumpButton.style.unityBackgroundImageTintColor = new StyleColor(Colors.GetColor(EColors.White));
 
 			switch (color)
 			{
@@ -86,7 +93,7 @@ namespace UI
 					_fireballButton.visible = true;
 					break;
 				case EColors.Green:
-					_jumpButton.style.color = new StyleColor(Colors.GetColor(EColors.Green));
+					_jumpButton.style.unityBackgroundImageTintColor = new StyleColor(Colors.GetColor(EColors.Green));
 					break;
 				case EColors.Blue:
 					_speedButton.visible = true;
